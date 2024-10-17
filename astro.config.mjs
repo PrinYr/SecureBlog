48a153802c8c8d4dc70ad74aeb8e7239e0396464
PrinYr <gapth33@gmail.com>
2024-10-18 06:34:14 +0700

    Add tailwind.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import cloudflare from '@astrojs/cloudflare';
 
 import react from '@astrojs/react';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: "https://secureblog.pages.dev/"
 });
